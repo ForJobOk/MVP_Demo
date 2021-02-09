02a1ca3efdf33e61163af6a0a73012558e6ccf57
ONOUEKENTO <kentou1506@gmail.com>
2021-02-10 01:05:40 +0900

    Add MusicPlayerView

---
+++ Assets/MusicPlayer/MusicPlayModeReactiveProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UniRx;
+
+namespace Ono.MVP.CustomRP
+{
+    /// <summary>
+    /// 音楽再生に関する状態
+    /// </summary>
+    public enum MusicPlayMode
+    {
+        Play,
+        Stop
+    }
+    
+    [Serializable]
+    public class MusicPlayModeReactiveProperty : ReactiveProperty<MusicPlayMode>
+    {
+        public MusicPlayModeReactiveProperty (){}
+        public MusicPlayModeReactiveProperty (MusicPlayMode initialValue) : base (initialValue) {}
+    }
+}
---
+++ Assets/MusicPlayer/MusicPlayerView.cs
@@ -0,0 +1,46 @@
+﻿using Ono.MVP.CustomRP;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ono.MVP.View
+{
+    /// <summary>
+    /// View
+    /// </summary>
+    public class MusicPlayerView : MonoBehaviour
+    {
+        [SerializeField] private Button _playButton, _stopButton;
+        [SerializeField] private Slider _seekBar;
+
+        /// <summary>
+        /// 音楽再生モードかどうか
+        /// 購読機能のみ外部に公開
+        /// </summary>
+        public IReadOnlyReactiveProperty<MusicPlayMode> MusicPlayModeRP => _musicPlayModeRP;
+        private readonly MusicPlayModeReactiveProperty _musicPlayModeRP = new MusicPlayModeReactiveProperty();
+        
+        private void Start()
+        {
+            //プレイボタン押下
+            _playButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    _playButton.gameObject.SetActive(false);
+                    _stopButton.gameObject.SetActive(true);
+                    //値の更新(発火)
+                    _musicPlayModeRP.Value = MusicPlayMode.Play;
+                }).AddTo(this);
+        
+            //停止ボタン押下
+            _stopButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    _playButton.gameObject.SetActive(true);
+                    _stopButton.gameObject.SetActive(false);
+                    //値の更新(発火)
+                    _musicPlayModeRP.Value = MusicPlayMode.Stop;
+                }).AddTo(this);
+        }
+    }
+}
